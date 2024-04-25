004f35019d5c037aecbb549acad9821648498b18
Akshay Matre <99554130+matrecode@users.noreply.github.com>
2024-04-25 11:13:32 +0000

    Fixed error

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -13,11 +13,10 @@ const Counter = () => {
   return (
     <>
       <h1>Counter App</h1>
-      <p>Counter: {count}</p>
-      <div>
-        <button onClick={handleIncrement}>Increment</button>
-        <button onClick={handleDecrement}>Decrement</button>
-      </div>
+      <p>Count: {count}</p>
+
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </>
   );
 };
